95561ad3662353ebc75dfe3e7e22afdc94f4be15
hamedfathi <hamedfathi@outlook.com>
2022-03-26 10:48:45 +0100

    chore(Extensions): Setting added.

--- PostmanToOpenApi/PostmanToOpenApiExtensions.cs
+++ PostmanToOpenApi/PostmanToOpenApiExtensions.cs
@@ -6,17 +6,45 @@ namespace PostmanToOpenApi
 {
     public static class PostmanToOpenApiExtensions
     {
-        public static OpenApiDocument ConvertTo(this PostmanCollection postmanCollection)
+        public static OpenApiDocument ToOpenApiDocument(this PostmanCollection postmanCollection, Setting setting = null)
         {
             if (postmanCollection is null)
             {
                 throw new ArgumentNullException(nameof(postmanCollection));
             }
-
+            setting = setting ?? new Setting();
             var openApiDocument = new OpenApiDocument();
+
+            openApiDocument.Info = postmanCollection.Info.ToOpenApiInfo(setting);
+
+
 
 
             return null;
+        }
+        public static OpenApiInfo ToOpenApiInfo(this Information information, Setting setting)
+        {
+            if (information is null)
+            {
+                throw new ArgumentNullException(nameof(information));
+            }
+
+            if (setting is null)
+            {
+                throw new ArgumentNullException(nameof(setting));
+            }
+
+            var info = new OpenApiInfo
+            {
+                Description = information.Description.HasValue
+                    ? information.Description.Value.String
+                    : null,
+                Version = information.Version.HasValue
+                    ? information.Version.Value.String
+                    : setting.DefaultVersion
+            };
+
+            return info;
         }
     }
 }
---
+++ PostmanToOpenApi/Setting.cs
@@ -0,0 +1,7 @@
+﻿namespace PostmanToOpenApi
+{
+    public class Setting
+    {
+        public string DefaultVersion { get; set; } = "0.0.1";
+    }
+}
