8f30e611a1aa754bf1a5e3ac618b88d654c8cad4
hamedfathi <hamedfathi@outlook.com>
2022-03-26 09:32:53 +0100

    feat(Project): Skeleton added.

---
+++ PostmanToOpenApi/PostmanToOpenApiExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+using PostmanCollectionReader;
+using System;
+
+namespace PostmanToOpenApi
+{
+    public static class PostmanToOpenApiExtensions
+    {
+        public static OpenApiDocument ConvertTo(this PostmanCollection postmanCollection)
+        {
+            if (postmanCollection is null)
+            {
+                throw new ArgumentNullException(nameof(postmanCollection));
+            }
+
+            var openApiDocument = new OpenApiDocument();
+
+
+
+            return null;
+        }
+    }
+}
